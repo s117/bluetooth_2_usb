2c999902cfc59141cde1c5225d05a546e0013eb8
quaxalber <64684396+quaxalber@users.noreply.github.com>
2025-03-18 19:16:02 +0100

    Update relay.py

--- src/bluetooth_2_usb/relay.py
+++ src/bluetooth_2_usb/relay.py
@@ -384,10 +384,7 @@ class DeviceRelay:
         async for input_event in self._input_device.async_read_loop():
             event = categorize(input_event)
 
-            if any(isinstance(event, ev_type) for ev_type in [KeyEvent, RelEvent]):
-                _logger.debug(
-                    f"Received {event} from {self._input_device.name} ({self._input_device.path})"
-                )
+            _logger.debug(f"Received {event} from {self._input_device.name} ({self._input_device.path})")
 
             if self._shortcut_toggler and isinstance(event, KeyEvent):
                 self._shortcut_toggler.handle_key_event(event)

7b496bbb91e1b09beb95b6c46c25cec6479bd1a9
Benjamin T <benfred+git@posteo.de>
2025-03-25 20:19:55 +0100

    Disable gamepad

--- src/bluetooth_2_usb/relay.py
+++ src/bluetooth_2_usb/relay.py
@@ -79,7 +79,7 @@ class GadgetManager:
                 Device.BOOT_MOUSE,
                 Device.KEYBOARD,
                 Device.CONSUMER_CONTROL,
-                Device.GAMEPAD,
+                # Device.GAMEPAD,
                 # Device.DIGITIZER,
             ]
         )  # type: ignore
@@ -88,7 +88,7 @@ class GadgetManager:
         self._gadgets["keyboard"] = Keyboard(enabled_devices)
         self._gadgets["mouse"] = Mouse(enabled_devices)
         self._gadgets["consumer"] = ConsumerControl(enabled_devices)
-        self._gadgets["gamepad"] = Gamepad(enabled_devices)
+        # self._gadgets["gamepad"] = Gamepad(enabled_devices)
         # self._gadgets["digitizer"] = Digitizer(enabled_devices)
         self._enabled = True
 

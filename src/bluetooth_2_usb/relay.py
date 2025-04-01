0e51779d931a75b114ce00ca82122beb7e4e5b45
Benjamin T <benfred+git@posteo.de>
2025-04-01 20:41:43 +0200

    Log absolute events

--- src/bluetooth_2_usb/relay.py
+++ src/bluetooth_2_usb/relay.py
@@ -424,7 +424,9 @@ class DeviceRelay:
         async for input_event in self._input_device.async_read_loop():
             event = categorize(input_event)
 
-            if any(isinstance(event, ev_type) for ev_type in [KeyEvent, RelEvent]):
+            if any(
+                isinstance(event, ev_type) for ev_type in [KeyEvent, RelEvent, AbsEvent]
+            ):
                 _logger.debug(
                     f"Received {event} from {self._input_device.name} ({self._input_device.path})"
                 )

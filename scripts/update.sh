961bee1c1ca328e2fc6b9e29fb0166e729464513
Benjamin T <benfred+git@posteo.de>
2025-10-14 20:02:14 +0200

    chore: remove test variables

--- scripts/update.sh
+++ scripts/update.sh
@@ -63,8 +63,6 @@ source "$VENV_DIR/bin/activate"
 python -V; pip install --upgrade pip wheel setuptools
 if [[ -f "${INSTALL_DIR}/requirements.txt" ]] ; then pip install -r "${INSTALL_DIR}/requirements.txt"; else warn "requirements.txt missing"; fi
 
-SERVICE_NAME="bluetooth_2_usb"
-RESTART=1
 if [[ $RESTART -eq 1 ]]; then
   if systemctl cat "${SERVICE_NAME}.service" >/dev/null 2>&1; then
     info "Restarting ${SERVICE_NAME}.service…"; systemctl restart "${SERVICE_NAME}.service"
